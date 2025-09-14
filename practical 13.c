a7c835d336cb8bdac37f2a67b94ad9e1ce89ab9d
panhapatel <patelpanha79@gmail.com>
2025-09-14 23:13:24 +0530

    Add files via upload

---
+++ practical 13.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int cwl=10;
+    for(cwl=10;cwl<=100;cwl=cwl+10){
+            printf("Current water level:%d liters\n",cwl);
+        }
+        printf("The tank is full");
+    return 0;
+}
